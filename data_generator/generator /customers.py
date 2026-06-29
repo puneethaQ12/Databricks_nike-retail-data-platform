2b2275fa367d076fc84c9167994f828837bc67af
Puneetha S <119097022+puneethaQ12@users.noreply.github.com>
2026-06-29 17:25:36 +0530

    Add customer data generation script
    This script generates a CSV file containing customer data with fields such as customer ID, name, email, phone, city, state, country, signup date, and status.

--- data_generator/generator /customers.py
+++ data_generator/generator /customers.py
@@ -1,1 +1,59 @@
+import os
+import random
+import pandas as pd
+from faker import Faker
 
+fake = Faker("en_IN")
+
+OUTPUT_FOLDER = "../datasets/raw"
+NUMBER_OF_CUSTOMERS = 1000
+
+
+def generate_customer_id(number):
+    return f"C{number:06d}"
+
+
+def generate_customers():
+
+    customers = []
+
+    for i in range(1, NUMBER_OF_CUSTOMERS + 1):
+
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+
+        customers.append({
+
+            "customer_id": generate_customer_id(i),
+
+            "first_name": first_name,
+
+            "last_name": last_name,
+
+            "email": f"{first_name.lower()}.{last_name.lower()}@gmail.com",
+
+            "phone": fake.msisdn()[:10],
+
+            "city": fake.city(),
+
+            "state": fake.state(),
+
+            "country": "India",
+
+            "signup_date": fake.date_between("-5y", "today"),
+
+            "customer_status": random.choice(["ACTIVE", "INACTIVE"])
+
+        })
+
+    df = pd.DataFrame(customers)
+
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+    df.to_csv(f"{OUTPUT_FOLDER}/customers.csv", index=False)
+
+    print(f"Generated {len(df)} customers")
+
+
+if __name__ == "__main__":
+    generate_customers()
